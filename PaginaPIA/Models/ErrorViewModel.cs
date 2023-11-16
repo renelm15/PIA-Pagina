0eb7764979a436506fb3438aeeb5c7f438c2cb14
Rene Lara <rene.lafita@gmail.com>
2023-11-16 12:50:33 -0600

    Archivos de la pagina

---
+++ PaginaPIA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaginaPIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
